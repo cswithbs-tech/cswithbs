709346363dead6877df617ace9070c1b2f23848d
shadows237 <165073025+blackcoder234@users.noreply.github.com>
2026-08-23 00:39:55 +0530

    feat: add ui-avatars.com to allowed image hostnames in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -23,6 +23,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "ui-avatars.com",
+      },
     ],
     dangerouslyAllowSVG: true,
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
